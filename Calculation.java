9b96fa344a1bd2ecebe061fd4ff8b5bd5d1b3c2e
snehasingh-2 <snehasingh272001@gmail.com>
2023-11-18 16:27:20 +0530

    done changes

---
+++ Calculation.java
@@ -0,0 +1,27 @@
+package day1;
+//wap to print sum of two number 
+
+public class Calculation {
+
+	public static void main(String[] args) {
+
+		//local variable 
+		int a=100;
+		int b= 200;
+		int c=a+b;
+		
+		System.out.println( "sum= " + c);
+		
+		//numeric datatype - int ,byte, short,long 
+		//floating           float double
+		
+		//short k= 32768; // values exceed out of range 
+		
+		// we cannot use local variable without assignment 
+		int u=2;
+		System.out.println(u);
+		
+
+	}
+
+}
