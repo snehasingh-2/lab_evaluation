f3c96058326d986b14f407425410d89ae637755d
snehasingh-2 <snehasingh272001@gmail.com>
2023-11-18 15:28:18 +0530

    Done Changes

---
+++ SwapNumbers.java
@@ -0,0 +1,14 @@
+package Assignment1;
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println("Before Swapping: a = " +a+ " and b =" +b);
+		int temp = a;
+		a = b;
+		b = temp;
+		System.out.println("After Swapping: a = " +a+ " and b =" +b);
+	}
+}
