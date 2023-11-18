a7ab594ae8120c8611a54166197b733d9d1616a7
snehasingh-2 <139580847+snehasingh-2@users.noreply.github.com>
2023-11-18 16:11:23 +0530

    Add files via upload

---
+++ HelloMessage.java
@@ -0,0 +1,18 @@
+package day1;
+//everything must be inside class
+//class name should be in uppercase 
+//to run program we should write main method 
+//classname = filename 
+
+//wap to print welcome message
+
+public class HelloMessage {
+
+	public static void main(String[] args) {
+		 
+
+		System.out.println("welcome to Pgdac ");
+
+	}
+
+}
