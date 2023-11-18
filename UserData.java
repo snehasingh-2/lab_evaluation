010ea5e12e3294fafbeff3ba53bfc06d92ae2efd
snehasingh-2 <139580847+snehasingh-2@users.noreply.github.com>
2023-11-18 17:05:24 +0530

    Update UserData.java

--- UserData.java
+++ UserData.java
@@ -1,22 +1,11 @@
-//wap to ask three numbers from user and print their sum 
-//java.util = package name 
-//Scanner = Class name 
-import java.util.Scanner;
-
 public class UserData {
 
 	public static void main(String[] args) {
 
 		int x,y,z;
-		//in line 14 we are creating object of scanner class
-		
-		Scanner s= new Scanner(System.in);
-		
-		System.out.println("enter three numbers ");
-		x=s.nextInt(); //nextInt is predefined method in scanner class
-		y=s.nextInt();
-		z=s.nextInt();
-		
+		x=10;
+		y=20;
+		z=30;
 		
 		int sum= x+y+z;
 		System.out.println("sum="+ sum );
